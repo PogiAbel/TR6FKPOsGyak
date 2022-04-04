5e4193fc8597e1a66a090cd3a823dd7e43a9478a
PogiAbel <pogonyiabel@gmail.com>
2022-04-04 20:41:05 +0200

    2. feladat befejezve

--- TR6FKP_0404/tr6fkp_tobbszignal.c
+++ TR6FKP_0404/tr6fkp_tobbszignal.c
@@ -2,17 +2,21 @@
 #include <signal.h>
 #include <unistd.h>
 void handleSignals(int sig){
-    printf("Signal: %d\n",sig);
+    if(sig == 3){
+        printf("Signal: %d\n",sig);
+    } else if(sig == SIGINT){
+        printf("Signal: %d\n",sig);
+    }
 }
 
 int main(){
     signal(SIGINT, handleSignals);
+    int pid =1;
     for (int i = 0; ;i++)
     {
         printf("Fo szal\n");
         sleep(1);
     }
-    
 
     return 0;
 }
