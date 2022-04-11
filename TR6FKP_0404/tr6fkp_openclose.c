89073509be2d4cb596425dcd269e2f557aa8075a
PogiAbel <pogonyiabel@gmail.com>
2022-04-11 11:01:58 -0400

    valami

--- TR6FKP_0404/tr6fkp_openclose.c
+++ TR6FKP_0404/tr6fkp_openclose.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(){
     FILE *f;
-    f = fopen("tr6fkp.txt","r");
+    f = fopen("tr6fkp.txt","r+");
 
     if (NULL == f){
         printf("Nem lehet a file-t megyitni");
+    }
+    else printf("Sikeres megnyitas");
+    if (write(1, "This will be output to testfile.txt\n", 36) != 36) {
+        write(2, "There was an error writing to testfile.txt\n", 43);
+        return -1;
     }
 
     return 0;
